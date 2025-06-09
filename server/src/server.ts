98257134eb593789cadd6c25e420be1f859d3c62
jcalderaro-96 <jcalderaro96@gmail.com>
2025-06-09 05:42:26 -0700

    Starter Code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 dotenv.config();
 
 // Import the routes
@@ -9,11 +10,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// Serve static files of entire client dist folder
+app.use(express.static(path.resolve('../client/dist')));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
+// Middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
